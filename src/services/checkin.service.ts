03c0478968c77c73c74244279874c205ac4f2be2
Younes El Yandouzi <elyandoy@iut1i-605-z15.iut1.local>
2017-02-16 13:59:15 +0100

    Added new methods to authentication and to get specific checkin

--- src/services/checkin.service.ts
+++ src/services/checkin.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
-import { Http }       from '@angular/http';
+import { Injectable }    from '@angular/core';
+import { Http, Headers } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
@@ -8,14 +8,47 @@ import { CheckIn } from './checkIn';
 
 @Injectable()
 export class CheckInService {
-
   private baseURL: string = "http://checkin-api.dev.cap-liberte.com/";
+  private authToken;
 
-  constructor(private http: Http) {}
+  constructor(private http: Http) {
+    this.http = http;
+    this.authToken = null;
+  }
 
-  list(): Observable<CheckIn[]> {
+  authenticate(user) {
+    var body = JSON.stringify(user);
+    var headers = new Headers({'Content-Type': 'x-www-form-urlencoded'});
+
+    return new Promise(resolve => {
+      this.http
+          .post(this.baseURL+`auth`, body, {headers: headers}).subscribe(data => {
+            if(data.json().token) {
+              var token = data.json().token;
+              this.authToken = token;
+              window.localStorage.setItem('token', token);
+              resolve(true);
+            } else {
+              resolve(false);
+            }
+      });
+    });
+  }
+
+  logout() {
+    this.authToken = null;
+    window.localStorage.removeItem('token');
+  }
+
+  getListCheckIn(): Observable<CheckIn[]> {
     return this.http
                .get(this.baseURL+`checkin`)
                .map(response => response.json() as CheckIn[]);
+  }
+
+  getCheckIn(id): Observable<CheckIn> {
+    return this.http
+               .get(this.baseURL+`checkin/`+id)
+               .map(response => response.json() as CheckIn);
   }
 }
