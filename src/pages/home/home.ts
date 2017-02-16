cdec3dfc0039796adc0938662991f91abd75274c
Younes El Yandouzi <elyandoy@iut1i-605-z15.iut1.local>
2017-02-16 14:06:14 +0100

    Reset home page as blank

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
-import { CheckInService } from '../../services/checkin.service';
-import { CheckIn } from '../../services/checkin';
 
 @Component({
   selector: 'page-home',
@@ -11,22 +7,8 @@ import { CheckIn } from '../../services/checkin';
 })
 export class HomePage {
   
-  checkin: Array<CheckIn>;
-
-  constructor(public navCtrl: NavController, private checkinService: CheckInService) {
+  constructor(public navCtrl: NavController) {
     
-  }
-
-  ngOnInit(): void {
-    this.checkinService.list().subscribe(
-        data => {
-            this.checkin = data;
-        },
-        err => {
-            console.log(err);
-        },
-        () => console.log('Get checkin completed')
-    );
   }
 
 }
