df1b34c2d851f2678e50fc01d7d24702baab140f
younes038 <younes.el-yandouzi@etu.univ-grenoble-alpes.fr>
2017-03-26 23:31:10 +0200

    Login page allows to register with modal page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,8 @@
 import { Component,  } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ModalController, ViewController} from 'ionic-angular';
+import { Geolocation, Camera } from 'ionic-native';
 
-import { HomePage } from '../home/home';
+import { CheckInPage } from '../checkin/checkin';
 import { CheckInService } from '../../services/checkin.service';
 
 /*
@@ -21,7 +22,7 @@ export class LoginPage {
     password: ''
   }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private checkinService: CheckInService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private checkinService: CheckInService, public modalCtrl: ModalController) {
     this.checkinService = checkinService;
   }
 
@@ -32,13 +33,122 @@ export class LoginPage {
   authenticate(user) {
     this.checkinService.authenticate(user).then(data => {
       if(data) {
-        this.navCtrl.setRoot(HomePage);
+        this.navCtrl.setRoot(CheckInPage);
       }
     });
   }
 
-  logout() {
-    this.checkinService.logout();
+  register() {
+    let modal = this.modalCtrl.create(Signup);
+    modal.present();
   }
 
+}
+
+
+@Component({
+  selector: 'page-signup',
+  template: `
+    <ion-header>
+      <ion-navbar>
+        <ion-title>S'inscrire</ion-title>
+        <ion-buttons end>
+          <button ion-button icon-only class="button button-icon" (click)="dismiss()">Fermer</button>
+        </ion-buttons>
+      </ion-navbar>
+    </ion-header>
+
+    <ion-content padding>
+      <form (Submit)="register()" #registerForm="ngForm">
+        <ion-list inset>
+          
+          <ion-item>
+            <ion-input type="text" placeholder="Nom" name="name" [(ngModel)]="user.name" required></ion-input>
+          </ion-item>
+
+          <ion-item>
+            <ion-input type="text" placeholder="Email" name="email" [(ngModel)]="user.email" required></ion-input>
+          </ion-item>
+          
+          <ion-item>
+            <ion-input type="password" placeholder="Password" name="password" [(ngModel)]="user.password" required></ion-input>
+          </ion-item>
+          
+          <ion-row>
+            <ion-col text-center>
+              Sélectionner une image:
+            </ion-col>
+          </ion-row>
+
+          <ion-row>
+            <ion-col text-center>
+              <img [src]="user.image" />
+            </ion-col>
+          </ion-row>
+
+          <ion-row>
+            <ion-col text-center>
+              <div round ion-button role="button button-icon button-md button-round-md" class="button button-icon" (click)="openGallery()">
+                <ion-icon name="images"></ion-icon>
+              </div>
+              <div round ion-button role="button button-icon button-md button-round-md" class="button button-icon" (click)="takePicture()">
+                <ion-icon name="camera"></ion-icon>
+              </div>
+            </ion-col>
+          </ion-row>
+
+          <ion-item>
+            <button ion-button class="submit-btn" full type="submit" [disabled]="!registerForm.form.valid">Confirmer</button>
+          </ion-item>
+
+        </ion-list>
+      </form>
+    </ion-content>
+  `
+})
+export class Signup {
+  user: any;
+
+  constructor(public navCtrl: NavController, public viewCtrl: ViewController, private checkinService: CheckInService) {
+    this.checkinService = checkinService;
+    this.user = {
+      name: '',
+      email: '',
+      password: '',
+      image: ''
+    };
+  }
+
+  takePicture(e){
+    Camera.getPicture({
+      destinationType: Camera.DestinationType.DATA_URL,
+      encodingType: Camera.EncodingType.JPEG,
+      mediaType: Camera.MediaType.PICTURE,
+      correctOrientation: true
+    }).then((image) => {
+        this.user.image = 'data:image/jpeg;base64,'+image;
+    }, (err) => {
+        console.log(err);
+    });
+  }
+
+  openGallery(){
+    Camera.getPicture({
+      sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: Camera.DestinationType.DATA_URL
+    }).then((image) => {
+        this.user.image = 'data:image/jpeg;base64,'+image;
+      }, (err) => {
+        console.log(err);
+    });   
+  }
+
+  signup(user) {
+    this.checkinService.signup(user);
+    this.navCtrl.setRoot(CheckInPage);
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
 }
