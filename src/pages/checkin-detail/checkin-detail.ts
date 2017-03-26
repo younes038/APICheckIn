4dff4cb03f52b77a8096d638104f972621803619
younes038 <younes.el-yandouzi@etu.univ-grenoble-alpes.fr>
2017-03-26 23:16:44 +0200

    Created a new page to see selected checkin's detail

---
+++ src/pages/checkin-detail/checkin-detail.ts
@@ -0,0 +1,81 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+import {
+GoogleMap,
+GoogleMapsEvent,
+GoogleMapsLatLng,
+CameraPosition,
+GoogleMapsMarkerOptions,
+GoogleMapsMarker,
+Geolocation
+} from 'ionic-native';
+
+import { CheckInService } from '../../services/checkin.service';
+import { CheckIn } from '../../services/checkin';
+
+/*
+  Generated class for the CheckinDetail page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-checkin-detail',
+  templateUrl: 'checkin-detail.html'
+})
+export class CheckinDetailPage {
+  checkin: CheckIn;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private checkinService: CheckInService) {
+    this.checkinService = checkinService;
+    this.checkinService.getCheckIn(this.navParams.get('id')).subscribe(
+        data => {
+            this.checkin = data;
+        },
+        err => {
+            console.log(err);
+        },
+        () => console.log('Get checkin completed')
+    );
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CheckinDetailPage');
+    //this.loadMap();
+  }
+
+  loadMap() {
+		// create a new map by passing HTMLElement
+		let element: HTMLElement = document.getElementById('carte');
+		let map = new GoogleMap(element);
+		
+		// listen to MAP_READY event
+		map.one(GoogleMapsEvent.MAP_READY).then(() => {
+			// get user current position
+			Geolocation.getCurrentPosition().then((pos) => {
+				// create LatLng object
+				let geoloc: GoogleMapsLatLng = new GoogleMapsLatLng(pos.coords.latitude, pos.coords.longitude);
+
+				// create CameraPosition
+				let position: CameraPosition = {
+					target: geoloc,
+					zoom: 13
+				};
+
+				// move the map's camera to position
+				map.moveCamera(position); // works on iOS and Android
+			}, (err) => {
+				console.log(err);
+			});
+
+      // create new marker
+      let markerOptions: GoogleMapsMarkerOptions = {
+        position: new GoogleMapsLatLng(this.checkin.lat, this.checkin.lng),
+        title: `Marker ${this.checkin.id}`
+      };
+
+      map.addMarker(markerOptions);
+		});
+	}
+}
