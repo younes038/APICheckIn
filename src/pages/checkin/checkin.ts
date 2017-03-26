fa5e2dae2891f2ce294e37306fce3de5fa4af9c9
younes038 <younes.el-yandouzi@etu.univ-grenoble-alpes.fr>
2017-03-26 23:36:30 +0200

    Checkins list is refreshable and populatable with modal page

--- src/pages/checkin/checkin.ts
+++ src/pages/checkin/checkin.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ModalController, ViewController } from 'ionic-angular';
+import { Camera, Geolocation } from 'ionic-native';
 
 import { CheckInService } from '../../services/checkin.service';
 import { CheckIn } from '../../services/checkin';
+import { CheckinDetailPage } from '../checkin-detail/checkin-detail';
 
 /*
   Generated class for the Checkin page.
@@ -15,18 +17,13 @@ import { CheckIn } from '../../services/checkin';
   templateUrl: 'checkin.html'
 })
 export class CheckInPage {
+  checkins: Array<CheckIn>
 
-  checkin: Array<CheckIn>
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, private checkinService: CheckInService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private checkinService: CheckInService, public modalCtrl: ModalController) {
     this.checkinService = checkinService;
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CheckinPage');
     this.checkinService.getListCheckIn().subscribe(
         data => {
-            this.checkin = data;
+            this.checkins = data;
         },
         err => {
             console.log(err);
@@ -35,4 +32,125 @@ export class CheckInPage {
     );
   }
 
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CheckinPage');
+  }
+
+  refreshList(refresher) {
+    this.checkinService.getListCheckIn().subscribe(
+      data => {
+          this.checkins = data;
+      }
+    );
+    refresher.complete();
+  }
+  
+  gotoDetail(id): void {
+    this.navCtrl.push(CheckinDetailPage, {id: id});
+  }
+
+  presentModal() {
+    let modal = this.modalCtrl.create(AddCheckin);
+    modal.present();
+  }
+
+}
+
+
+@Component({
+  selector: 'page-addCheckin',
+  template: `
+    <ion-header>
+      <ion-navbar>
+        <ion-title>Ajouter un checkin</ion-title>
+        <ion-buttons end>
+          <button ion-button icon-only class="button button-icon" (click)="dismiss()">Fermer</button>
+        </ion-buttons>
+      </ion-navbar>
+    </ion-header>
+
+    <ion-content padding>
+      <ion-grid>
+        <ion-row>
+          <ion-col text-center>
+            Sélectionner une image:
+          </ion-col>
+        </ion-row>
+        
+        <ion-row>
+          <ion-col text-center>
+            <img [src]="checkin.image" />
+          </ion-col>
+        </ion-row>
+
+        <ion-row>
+          <ion-col text-center>
+            <div round ion-button role="button button-icon button-md button-round-md" class="button button-icon" (click)="openGallery()">
+              <ion-icon name="images"></ion-icon>
+            </div>
+            <div round ion-button role="button button-icon button-md button-round-md" class="button button-icon" (click)="takePicture()">
+              <ion-icon name="camera"></ion-icon>
+            </div>
+          </ion-col>
+        </ion-row>
+
+        <ion-row>
+          <ion-col text-center>
+            <button ion-button block class="button button-icon" (click)="addCheckin()">Confirmer</button>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+    </ion-content>
+  `
+})
+export class AddCheckin {
+  checkin;
+
+  constructor(public viewCtrl: ViewController, private checkinService: CheckInService) {
+    this.checkin = {
+      lat: 0,
+      lng: 0,
+      image: ''
+    };
+    this.checkinService = checkinService;
+    Geolocation.getCurrentPosition().then((pos) => {
+      this.checkin.lat = pos.coords.latitude;
+      this.checkin.lng = pos.coords.longitude;
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  addCheckin(image) {
+    this.checkin.image = image;
+    this.checkinService.addCheckIn(this.checkin);
+  }
+
+  takePicture(){
+    Camera.getPicture({
+      destinationType: Camera.DestinationType.DATA_URL,
+      encodingType: Camera.EncodingType.JPEG,
+      mediaType: Camera.MediaType.PICTURE,
+      correctOrientation: true
+    }).then((image) => {
+        this.checkin.image = 'data:image/jpeg;base64,'+image;
+    }, (err) => {
+        console.log(err);
+    });
+  }
+
+  gotoGallery(){
+    Camera.getPicture({
+      sourceType: Camera.PictureSourceType.SAVEDPHOTOALBUM,
+      destinationType: Camera.DestinationType.DATA_URL
+    }).then((image) => {
+        this.checkin.image = 'data:image/jpeg;base64,'+image;
+      }, (err) => {
+        console.log(err);
+    });   
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
 }
